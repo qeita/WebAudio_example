b584b2ca6ed744994657103e3a666f9d79db5be6
qei <kei.yagi@gmail.com>
2018-09-11 23:38:40 +0900

    add examples for 'Running the oscillator'

--- examples/assets/js/demo_02.js
+++ examples/assets/js/demo_02.js
@@ -15,8 +15,16 @@
 
   oscillator.type = 'sine'
   oscillator.frequency.value = 440
-  oscillator.connect( context.destination )
-  oscillator.start()
+
+  let gain = context.createGain()
+  oscillator.connect( gain )
+  gain.connect( context.destination )
+
+  let now = context.currentTime
+  gain.gain.setValueAtTime(1, now)
+  gain.gain.exponentialRampToValueAtTime(0.001, now + 0.5)
+  oscillator.start(now)
+  oscillator.stop(now + 0.5)
 
 
 })()
